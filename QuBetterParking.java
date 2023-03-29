5da8fdd9614eff89199a83713a239f346310d7cc
Chris Anzilotti <anzilottichr@gmail.com>
2023-03-29 15:27:24 -0400

    Printed the array and added a counter

--- QuBetterParking.java
+++ QuBetterParking.java
@@ -2,11 +2,34 @@ public class QuBetterParking
 {
     public static void main(String[] args) 
     {
-        //Made variables that hold the size of the array, the values can be changed to whatever we'd like
+        //Made variables that hold the size of the array, the values can be changed to whatever programmer wants
         int rowSize = 5; 
         int colSize = 5;
         
-        int [][] parking;
-        parking = new int[rowSize][colSize];
+        //Creating the array
+        char [][] parking;
+        parking = new char[rowSize][colSize];
+
+        //Counting variable for parking spaces
+        int availableSpots = 0;
+        //Printing Array and setting available spots
+        for(int row = 0; row < rowSize; row++)
+        {
+            for(int col = 0; col < colSize; col++)
+            {
+                if(parking[row][col] == parking[0][0])
+                {
+                    //Setting [0][0] to the exit
+                    parking[0][0] = 'E';
+                    System.out.print(parking[row][col] + " ");
+                    continue;
+                }
+                parking[row][col] = '0';
+                System.out.print(parking[row][col] + " ");
+                availableSpots++;
+            }
+            System.out.println();
+        }
+        System.out.println("Available spots: " + availableSpots);
     }
 }
