b94568b07dfa794ff65807eb07da381162baeac1
Chris Anzilotti <anzilottichr@gmail.com>
2023-03-29 15:05:51 -0400

    Added variables and array

--- QuBetterParking.java
+++ QuBetterParking.java
@@ -1,5 +1,12 @@
-public class QuBetterParking {
-    public static void main(String[] args) {
+public class QuBetterParking 
+{
+    public static void main(String[] args) 
+    {
+        //Made variables that hold the size of the array, the values can be changed to whatever we'd like
+        int rowSize = 5; 
+        int colSize = 5;
         
+        int [][] parking;
+        parking = new int[rowSize][colSize];
     }
 }
