fd87792277f28ba81e334c8441aa0d3a4bd60449
sloanelauren <sloanelaurenm@gmail.com>
2023-04-10 19:52:14 -0400

    parking ticket

--- QuBetterParking.java
+++ QuBetterParking.java
@@ -37,8 +37,11 @@ public class QuBetterParking
             for(int j = 0; j < colSize; j++){
                 if(parking[i][j] == '0'){
                     // i and j are the coordinates of the first available spot
-                    // to print the ticket this print statement needs to be made to look all formal 
-                    System.out.println(i + ", " + j);
+                    // Ticket statement (assigns parking spots)
+                    System.out.println("Please park in the ticketed spot.");
+                    System.out.println(" ------------------------");
+                    System.out.println("|      Spot : "+ i + j +"         |");
+                    System.out.println(" ------------------------");
                     // setting i and j to rowSize and colSize breaks the loop - a break could also be used here
                     i = rowSize;
                     j = colSize;
